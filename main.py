03da02571a2a00a384bd4144f75341ecf96cd8b7
Evan Luo <contact@ohevan.com>
2024-01-04 01:01:23 -0500

    feat: add async request to busuanzi in main.py and sync_busuanzi_data.py
    This commit adds a new feature which sends an asynchronous request to
    busuanzi. The changes are made in the main.py file where the function
    root is now an async function and it creates a task to send a request to
    busuanzi. A new file sync_busuanzi_data.py is added which contains the
    function to send the request. The httpx library is also added to the
    requirements.txt file to support this feature.

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ from fastapi.responses import FileResponse
 
 from get_before_data import get_page_pv_before, get_site_pv_before, get_site_uv_before
 from pv import update_site_pv, update_page_pv
+from sync_busuanzi_data import send_busuanzi_request
 from uv import update_site_uv
+import asyncio
 
 app = FastAPI(docs_url=None, redoc_url=None)
 
@@ -23,7 +25,9 @@ async def serve_css():
     return FileResponse("statics/css/style.css")
 
 @app.get("/")
-def root(request: Request, referer: str = Header(None), jsonpCallback: str = ""):
+async def root(request: Request, referer: str = Header(None), jsonpCallback: str = ""):
+    logging.info(f"New request received: referer={referer}, jsonpCallback={jsonpCallback}")
+
     if not referer:
         return FileResponse("statics/home.html")
 
@@ -31,17 +35,22 @@ def root(request: Request, referer: str = Header(None), jsonpCallback: str = "")
     parsed_url = urlparse(referer)
     host = parsed_url.netloc
     path = parsed_url.path.rstrip('/index')
-    logging.debug(f"host: {host}, path: {path}, client_host: {client_host}")
+
+    logging.info(f"Request parsed: host={host}, path={path}, client_host={client_host}")
 
     site_uv_before = get_site_uv_before(host, path)
     site_pv_before = get_site_pv_before(host, path)
     page_pv_before = get_page_pv_before(host, path)
 
-    logging.debug(f"host: {host}, path: {path}, site_uv_before: {site_uv_before}, site_pv_before: {site_pv_before}, page_pv_before: {page_pv_before}")
+    logging.info(f"Statistics before update: host={host}, path={path}, site_uv_before={site_uv_before}, site_pv_before={site_pv_before}, page_pv_before={page_pv_before}")
 
     site_uv = update_site_uv(host, client_host) + site_uv_before
     site_pv = update_site_pv(host, client_host) + site_pv_before
     page_pv = update_page_pv(host, path) + page_pv_before
+
+    logging.info(f"Updated statistics: host={host}, path={path}, site_uv={site_uv}, site_pv={site_pv}, page_pv={page_pv}")
+
+    asyncio.create_task(send_busuanzi_request(host, path))
 
     dict_data = {
         "site_uv": site_uv,
@@ -50,6 +59,8 @@ def root(request: Request, referer: str = Header(None), jsonpCallback: str = "")
         "version": 2.4
     }
     data_str = f"try{{{jsonpCallback}({json.dumps(dict_data)});}}catch(e){{}}"
+    logging.debug(f"Sending response data: {data_str}")
+
     return Response(content=data_str, media_type="application/javascript")
 
 if __name__ == "__main__":
