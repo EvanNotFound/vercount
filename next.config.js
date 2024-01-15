8ff9a5dc82ba74484940c971d28c64d682973c2f
Evan Luo <contact@ohevan.com>
2024-01-14 22:24:10 -0500

    feat: add CORS headers and reformat HTML
    - Add CORS headers to all API routes in `next.config.js`
    - Reformat HTML in `home.html` for better readability
    - Move `home.html` from `statics` to `public` directory

--- next.config.js
+++ next.config.js
@@ -6,6 +6,25 @@ const nextConfig = {
       destination: "/js/client.min.js",
     },
   ],
+  headers: async () => [
+    {
+      // matching all API routes
+      source: "/log",
+      headers: [
+        { key: "Access-Control-Allow-Credentials", value: "true" },
+        { key: "Access-Control-Allow-Origin", value: "*" },
+        {
+          key: "Access-Control-Allow-Methods",
+          value: "GET,DELETE,PATCH,POST,PUT",
+        },
+        {
+          key: "Access-Control-Allow-Headers",
+          value:
+            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+        },
+      ],
+    },
+  ],
 };
 
 module.exports = nextConfig;
