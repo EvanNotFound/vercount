240cff4a22d5b474bfd67d7dc48065fe587cd7ac
Evan Luo <contact@ohevan.com>
2024-01-31 22:50:48 -0500

    feat: add rate limiting middleware
    This commit introduces a new middleware function for rate limiting using
    @upstash/ratelimit. It also includes necessary changes in package.json and
    pnpm-lock.yaml for the new dependency. An example environment file (.env.example)
    has also been added. #2

---
+++ middleware.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+import { Ratelimit } from "@upstash/ratelimit";
+import { kv } from "@vercel/kv";
+
+const ratelimit = new Ratelimit({
+  redis: kv,
+  limiter: Ratelimit.slidingWindow(300, "1 h"),
+});
+
+// Define which routes you want to rate limit
+export const config = {
+  matcher: "/log",
+};
+
+export default async function middleware(request: NextRequest) {
+  const ip = request.ip ?? "127.0.0.1";
+  const { success, pending, limit, reset, remaining } = await ratelimit.limit(
+    ip,
+  );
+  return success
+    ? NextResponse.next()
+    : NextResponse.json(
+        {
+          error: "Rate limit exceeded",
+        },
+        {
+          status: 429,
+        },
+      );
+}
